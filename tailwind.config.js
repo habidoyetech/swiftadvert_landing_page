45378c94f4703f9d06eee81bcf719a66df073673
habidoyetech <abidoyeabiodun97@gmail.com>
2023-11-25 09:10:58 +0100

    create navbar

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,7 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require('tailwindcss/defaultTheme');
 module.exports = {
+  // prefix: 'bar-',
   content: [
     './pages/**/*.{js,ts,jsx,tsx,mdx}',
     './components/**/*.{js,ts,jsx,tsx,mdx}',
@@ -7,6 +9,15 @@ module.exports = {
   ],
   theme: {
     extend: {
+      fontFamily: {
+        mons: ['Montserrat', ...defaultTheme.fontFamily.sans],
+        opensan: ['Open sans', ...defaultTheme.fontFamily.sans],
+        dmsans: ['"DM Sans"', ...defaultTheme.fontFamily.sans]
+      },
+      colors: {
+        'primary-color': 'rgba(90,11,77,1)',
+        'transparent-white': 'rgb(248, 248, 248)'
+      },
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
